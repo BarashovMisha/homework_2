53a9f9ce1e11fc6aaa91d580f75b021820b44e2c
BarashovMisha <barashov.m@edex.online>
2024-12-25 21:39:11 +0500

    Загрузили файл для домашнего задания

---
+++ main.py
@@ -0,0 +1,7 @@
+lists = [1, 2, 'hello'] # изменяемые
+tuples = (1, 2, 'hello') # неизменяемые
+sets = {1, 2, 'hello', 1} # хранит только уникальное значение
+
+dicts = {'name': "Misha"}
+dicts['age'] = 12
+print(dicts)
